9d95f4599ec1587eccd6c493387934042f19f47b
Paul Campagner <mcampagner@cloud3tech.com>
2026-03-24 16:24:57 -0300

    Agregado el otro INA al modo detect

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,8 @@ const char* PASSWORD = "Lady-350!";
 // ─── Sensores ────────────────────────────
 Adafruit_SHT31   sht31;
 Adafruit_BMP085  bmp;
-Adafruit_INA219 ina219(0x41);
+Adafruit_INA219 ina219_solar(0x41);
+Adafruit_INA219 ina219_system(0x40);
 
 WebServer server(80);
 
@@ -26,9 +27,12 @@ void handleRoot() {
   float temp_bmp = bmp.readTemperature();
   float pres_abs = bmp.readPressure() / 100.0;
   float pres_qnh = bmp.readSealevelPressure(ALTITUD_M) / 100.0;
-  float voltage  = ina219.getBusVoltage_V();
-  float current  = ina219.getCurrent_mA();
-  float power    = ina219.getPower_mW();
+  float voltage_solar  = ina219_solar.getBusVoltage_V();
+  float current_solar  = ina219_solar.getCurrent_mA();
+  float power_solar    = ina219_solar.getPower_mW();
+  float voltage_system = ina219_system.getBusVoltage_V();
+  float current_system = ina219_system.getCurrent_mA();
+  float power_system   = ina219_system.getPower_mW();
 
   String body = "=== Estacion Meteorologica ===\n\n";
 
@@ -41,10 +45,15 @@ void handleRoot() {
   body += "  Presion abs : " + String(pres_abs, 1) + " hPa\n";
   body += "  Presion QNH : " + String(pres_qnh, 1) + " hPa\n\n";
 
-  body += "[INA219]\n";
-  body += "  Voltaje     : " + String(voltage, 3) + " V\n";
-  body += "  Corriente   : " + String(current, 1) + " mA\n";
-  body += "  Potencia    : " + String(power, 1) + " mW\n\n";
+  body += "[INA219 Solar]\n";
+  body += "  Voltaje     : " + String(voltage_solar, 3) + " V\n";
+  body += "  Corriente   : " + String(current_solar, 1) + " mA\n";
+  body += "  Potencia    : " + String(power_solar, 1) + " mW\n\n";
+
+  body += "[INA219 System]\n";
+  body += "  Voltaje     : " + String(voltage_system, 3) + " V\n";
+  body += "  Corriente   : " + String(current_system, 1) + " mA\n";
+  body += "  Potencia    : " + String(power_system, 1) + " mW\n\n";
 
   body += "==============================\n";
 
@@ -60,7 +69,8 @@ void setup() {
   // Sensores
   sht31.begin(0x44)  ? Serial.println("[OK] SHT31")   : Serial.println("[ERROR] SHT31");
   bmp.begin()        ? Serial.println("[OK] BMP180")  : Serial.println("[ERROR] BMP180");
-  ina219.begin()     ? Serial.println("[OK] INA219")  : Serial.println("[ERROR] INA219");
+  ina219_solar.begin()     ? Serial.println("[OK] INA219_Solar")  : Serial.println("[ERROR] INA219_Solar");
+  ina219_system.begin()    ? Serial.println("[OK] INA219_System") : Serial.println("[ERROR] INA219_System");
 
   // WiFi
   Serial.printf("Conectando a %s", SSID);
